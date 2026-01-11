c77c08fd93bda3a4ee0dfb9198ba19bc6248b198
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:15:41 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "traffic_manager_azure_endpoints" {
+  description = "All traffic_manager_azure_endpoint resources"
+  value       = azurerm_traffic_manager_azure_endpoint.traffic_manager_azure_endpoints
+}
+
---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "traffic_manager_azure_endpoints" {
+  description = <<EOT
+Map of traffic_manager_azure_endpoints, attributes below
+Required:
+    - name
+    - profile_id
+    - target_resource_id
+Optional:
+    - always_serve_enabled
+    - enabled
+    - geo_mappings
+    - priority
+    - weight
+    - custom_header (block):
+        - name (required)
+        - value (required)
+    - subnet (block):
+        - first (required)
+        - last (optional)
+        - scope (optional)
+EOT
+
+  type = map(object({
+    name                 = string
+    profile_id           = string
+    target_resource_id   = string
+    always_serve_enabled = optional(bool, false)
+    enabled              = optional(bool, true)
+    geo_mappings         = optional(list(string))
+    priority             = optional(number)
+    weight               = optional(number, 1)
+    custom_header = optional(object({
+      name  = string
+      value = string
+    }))
+    subnet = optional(object({
+      first = string
+      last  = optional(string)
+      scope = optional(number)
+    }))
+  }))
+}
+
